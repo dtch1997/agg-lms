7a87bcf6eec40106678b34e962032e13f63519e2
Daniel Tan <dtch1997@users.noreply.github.com>
2024-11-18 12:50:38 +0000

    add optimiser

---
+++ agg_lms/optimiser.py
@@ -0,0 +1,42 @@
+import inspect 
+import torch
+
+from agg_lms.core import Model
+
+def configure_optimisers(
+    model: Model, 
+    weight_decay: float, 
+    learning_rate: float, 
+    betas: tuple[float, float], 
+    device_type: str
+):
+    
+    # start with all of the candidate parameters
+    param_dict = {pn: p for pn, p in model.named_parameters()}
+    # filter out those that do not require grad
+    param_dict = {pn: p for pn, p in param_dict.items() if p.requires_grad}
+
+    # create optim groups. Any parameters that is 2D will be weight decayed, otherwise no.
+    # i.e. all weight tensors in matmuls + embeddings decay, all biases and layernorms don't.
+    decay_params = [p for _, p in param_dict.items() if p.dim() >= 2]
+    nodecay_params = [p for _, p in param_dict.items() if p.dim() < 2]
+    optim_groups = [
+        {'params': decay_params, 'weight_decay': weight_decay},
+        {'params': nodecay_params, 'weight_decay': 0.0}
+    ]
+    num_decay_params = sum(p.numel() for p in decay_params)
+    num_nodecay_params = sum(p.numel() for p in nodecay_params)
+    print(f"num decayed parameter tensors: {len(decay_params)}, with {num_decay_params:,} parameters")
+    print(f"num non-decayed parameter tensors: {len(nodecay_params)}, with {num_nodecay_params:,} parameters")
+
+    # Create AdamW optimizer 
+    # NOTE: The fused version of AdamW is faster
+    # See: https://pytorch.org/docs/stable/generated/torch.optim.AdamW.html
+    # The below code checks if the fused version is available and uses it if it is
+    fused_available = 'fused' in inspect.signature(torch.optim.AdamW).parameters
+    use_fused = fused_available and device_type == 'cuda'
+    extra_args = dict(fused=True) if use_fused else dict()
+    optimizer = torch.optim.AdamW(optim_groups, lr=learning_rate, betas=betas, **extra_args)
+    print(f"using fused AdamW: {use_fused}")
+
+    return optimizer
